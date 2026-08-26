166e9a15303bcddc4f7c346fa0b769332bbe6d78
MrNemod <coli-avatar-2002@hotmail.com>
2026-08-26 15:23:08 -0600

    Proyecto iniciado y apps agregadas

---
+++ Restaurant/Mesas_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MesasAppConfig(AppConfig):
+    name = 'Mesas_app'
---
+++ Restaurant/Mesero_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MeseroAppConfig(AppConfig):
+    name = 'Mesero_app'
