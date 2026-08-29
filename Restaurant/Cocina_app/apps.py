dc6c42e53061848583037b00e403856042b15435
MrNemod <coli-avatar-2002@hotmail.com>
2026-08-29 12:04:59 -0600

    Reestructura arquitectura de apps

--- Restaurant/Cocina_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CocinaAppConfig(AppConfig):
-    name = 'Cocina_app'
--- Restaurant/Mesero_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeseroAppConfig(AppConfig):
-    name = 'Mesero_app'
