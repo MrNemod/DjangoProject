9293a5ea22bac80524b8ce717770795fc7c98ad5
MrNemod <coli-avatar-2002@hotmail.com>
2026-09-01 14:41:35 -0600

    Base de datos iniciada con modelos de Menu

--- Restaurant/Menu/admin.py
+++ Restaurant/Menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import allergens, item, category
 
 # Register your models here.
+admin.site.register(allergens)
+admin.site.register(item)
+admin.site.register(category)
--- Restaurant/Menu/models.py
+++ Restaurant/Menu/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class allergens(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Alergenos"
+        verbose_name = "Alergeno"
+
+class category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categorias"
+        verbose_name = "Categoria"
+
+class item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    allergens = models.ManyToManyField(allergens)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Items"
+        verbose_name = "Item"
